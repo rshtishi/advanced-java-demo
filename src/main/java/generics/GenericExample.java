c1a28d6b84ebee8640ac93163b94b963b149104b
rshtishi <rando.shtishi@gmail.com>
2021-12-29 00:45:30 +0100

    Added example with bounded type parameters generics

--- src/main/java/generics/GenericExample.java
+++ src/main/java/generics/GenericExample.java
@@ -1,14 +1,17 @@
 package generics;
 
 import java.io.File;
+import java.util.Comparator;
 
 public class GenericExample {
 
     public static void main(String[] args) {
+
         //SimpleGenericMethod Examle
         simpleGenericMethod("Rando");
         simpleGenericMethod(new Object());
         simpleGenericMethod(new File(""));
+
         //SimpleGeneric Example
         SimpleGeneric<String> stringGeneric = new SimpleGeneric<>("Rando");
         stringGeneric.printType();
@@ -18,9 +21,26 @@ public class GenericExample {
 
         SimpleGeneric<File> fileGeneric = new SimpleGeneric<>(new File(""));
         fileGeneric.printType();
+
+        //Bounded Type Parameters
+        System.out.println("Max:" + findMaximum(new Integer(1), new Integer(2)));
+        System.out.println("Max:" + findMaximum("Rando", "Shtishi"));
+
     }
 
     public static <T> void simpleGenericMethod(T param) {
-        System.out.println("Type: " +param.getClass().getName());
+        System.out.println("Type: " + param.getClass().getName());
     }
+
+    public static <T extends Comparable<T>> T findMaximum(T a, T b) {
+        if (a == null) {
+            return b;
+        }
+        if (b == null) {
+            return a;
+        }
+        return a.compareTo(b) > 0 ? a : b;
+    }
+
+
 }
