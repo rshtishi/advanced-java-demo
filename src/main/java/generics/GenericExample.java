5507405a15687c80948243be220bfda68868457f
rshtishi <rando.shtishi@gmail.com>
2021-12-28 23:56:39 +0100

    Added example with generic method

--- src/main/java/generics/GenericExample.java
+++ src/main/java/generics/GenericExample.java
@@ -4,17 +4,23 @@ import java.io.File;
 
 public class GenericExample {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        //SimpleGenericMethod Examle
+        simpleGenericMethod("Rando");
+        simpleGenericMethod(new Object());
+        simpleGenericMethod(new File(""));
         //SimpleGeneric Example
         SimpleGeneric<String> stringGeneric = new SimpleGeneric<>("Rando");
         stringGeneric.printType();
 
-        SimpleGeneric<Object>  objectGeneric = new SimpleGeneric<>(new Object());
+        SimpleGeneric<Object> objectGeneric = new SimpleGeneric<>(new Object());
         objectGeneric.printType();
 
         SimpleGeneric<File> fileGeneric = new SimpleGeneric<>(new File(""));
         fileGeneric.printType();
+    }
 
-
+    public static <T> void simpleGenericMethod(T param) {
+        System.out.println("Type: " +param.getClass().getName());
     }
 }
