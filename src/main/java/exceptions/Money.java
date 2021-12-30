a021f2441576734177820b51fb21f0ca56b99701
rshtishi <rando.shtishi@gmail.com>
2021-12-30 17:08:06 +0100

    Minor modification

--- src/main/java/exceptions/Money.java
+++ src/main/java/exceptions/Money.java
@@ -2,12 +2,17 @@ package exceptions;
 
 public class Money {
 
-    public Money(int amount, String currency){
-        if(amount<0){
+    private int amount;
+    private String currency;
+
+    public Money(int amount, String currency) {
+        if (amount < 0) {
             throw new MoneyAmountException();
         }
-        if(currency!=null && !currency.isEmpty()){
+        if (currency != null && !currency.isEmpty()) {
             throw new MoneyCurrencyException();
         }
+        this.amount = amount;
+        this.currency = currency;
     }
 }
