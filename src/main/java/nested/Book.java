a0d9377103e930a679b93eb1a18427fc9ca343e7
rshtishi <rando.shtishi@gmail.com>
2022-01-16 12:33:27 +0100

    Adding builder pattern example

---
+++ src/main/java/nested/Book.java
@@ -0,0 +1,124 @@
+package nested;
+
+public class Book {
+
+    private String title;
+    private String author;
+
+    private String isbn;
+    private String genre;
+    private String published;
+    private String description;
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Book{");
+        sb.append("title='").append(title).append('\'');
+        sb.append(", author='").append(author).append('\'');
+        sb.append(", isbn='").append(isbn).append('\'');
+        sb.append(", genre='").append(genre).append('\'');
+        sb.append(", published='").append(published).append('\'');
+        sb.append(", description='").append(description).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPublished() {
+        return published;
+    }
+
+    public void setPublished(String published) {
+        this.published = published;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private Book(Builder builder){
+        this.title = builder.title;
+        this.author = builder.author;
+        this.isbn = builder.isbn;
+        this.published = builder.published;
+        this.genre = builder.genre;
+        this.description = builder.description;
+    }
+
+
+    public static class Builder {
+
+        private String title;
+        private String author;
+
+        private String isbn;
+        private String genre;
+        private String published;
+        private String description;
+
+        public Builder(String title, String author){
+            this.title = title;
+            this.author=author;
+        }
+
+        public Builder isbn(String isbn){
+            this.isbn = isbn;
+            return this;
+        }
+
+        public Builder genre(String genre){
+            this.genre = genre;
+            return this;
+        }
+
+        public Builder published(String published){
+            this.published = published;
+            return this;
+        }
+
+        public Builder description(String description){
+            this.description = description;
+            return this;
+        }
+
+        public Book build(){
+            return new Book(this);
+        }
+    }
+
+}
--- src/main/java/nested/NestedMain.java
+++ src/main/java/nested/NestedMain.java
@@ -18,7 +18,7 @@ public class NestedMain {
 
         System.out.println();
 
-        System.out.println(outer.callLocalInnerAnonymous(5.0,1.0));
+        System.out.println(outer.callLocalInnerAnonymous(5.0, 1.0));
 
         Outer.StaticInner staticInner = new Outer.StaticInner();
         staticInner.print();
@@ -31,6 +31,24 @@ public class NestedMain {
         System.out.println();
         Company.Receipt receipt = new Company("OSHE").new Receipt(45);
         receipt.print();
+
+        //Book book =
+        Book.Builder builder = new Book.Builder("Mizantrop", "Balzak")
+                .isbn("454545");
+        Book book1 = builder.genre("Classic").build();
+
+        Book book = new Book.Builder("","").build();
+
+        Book book2 = new Book.Builder("Metamorfoza", "Franz Kafka").build();
+
+        Book book3 = new Book.Builder("Huaji", "Albert Kamy")
+                .published("2020").build();
+
+        System.out.println();
+        System.out.println(book1);
+        System.out.println(book2);
+        System.out.println(book3);
+
     }
 
 }
